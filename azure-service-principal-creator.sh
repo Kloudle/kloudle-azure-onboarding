809a875ed6c803784f8f63ab59d63d00e91e4ccc
Riyaz Walikar <riyazwalikar@gmail.com>
2022-12-14 16:22:24 +0530

    Added app perms to read Azure AD objs

--- azure-service-principal-creator.sh
+++ azure-service-principal-creator.sh
@@ -25,6 +25,23 @@ export TENANT_ID=$(echo $RAW | jq .tenant | tr -d '"')
 az role assignment create --role "Reader and Data Access" --scope "$SCOPE" --assignee $APP_ID --output none
 az role assignment create --role "Security Reader" --scope "$SCOPE" --assignee $APP_ID --output none
 
+# Microsoft Graph APIs Application Role Access
+
+az ad app permission add --id $APP_ID --api 00000003-0000-0000-c000-000000000000 --api-permissions 7ab1d382-f21e-4acd-a863-ba3e13f7da61=Role # "Directory.Read.All"
+
+az ad app permission grant --id $APP_ID --api 00000003-0000-0000-c000-000000000000 --scope Directory.Read.All
+
+# Windows Azure Active Directory Application Role Access
+
+az ad app permission add --id $APP_ID --api 00000002-0000-0000-c000-000000000000 --api-permissions 3afa6a7d-9b1a-42eb-948e-1650a849e176=Role # "Application.Read.All"
+az ad app permission add --id $APP_ID --api 00000002-0000-0000-c000-000000000000 --api-permissions 5778995a-e1bf-45b8-affa-663a9f3f4d04=Role # "Directory.Read.All"
+
+az ad app permission grant --id $APP_ID --api 00000002-0000-0000-c000-000000000000 --scope Application.Read.All,Directory.Read.All
+
+# Grant Admin Consent so that the Principal can work through CLI
+
+az ad app permission admin-consent --id $APP_ID
+
 # Print data, provide this to Kloudle
 echo "tenant_id = $TENANT_ID"
 echo "subscription_id = $SUBSCRIPTION_ID"
